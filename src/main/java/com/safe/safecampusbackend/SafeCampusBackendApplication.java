c98dad8f4f0fc31cf8edd48ee2125c1ef1fa2ee5
yawnhaaa <yawnhaaa@163.com>
2024-03-11 12:29:09 +0800

    feat: 创建反诈校园后端

---
+++ src/main/java/com/safe/safecampusbackend/SafeCampusBackendApplication.java
@@ -0,0 +1,14 @@
+package com.safe.safecampusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeCampusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeCampusBackendApplication.class, args);
+        System.out.println("--------------------------------反诈校园，启动！--------------------------------");
+    }
+
+}
---
+++ src/test/java/com/safe/safecampusbackend/SafeCampusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safe.safecampusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeCampusBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
