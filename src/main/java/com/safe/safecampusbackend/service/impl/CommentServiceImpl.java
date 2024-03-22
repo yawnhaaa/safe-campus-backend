b174f5dda9d581e34d2c0f06c4e2f978fa2963b2
yawnhaaa <yawnhaaa@163.com>
2024-03-22 21:40:04 +0800

    评论功能第一稿

--- src/main/java/com/safe/safecampusbackend/service/impl/CommentServiceImpl.java
+++ src/main/java/com/safe/safecampusbackend/service/impl/CommentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.safe.safecampusbackend.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.safe.safecampusbackend.dao.CommentDAO;
 import com.safe.safecampusbackend.dao.InfoDAO;
 import com.safe.safecampusbackend.model.dto.CommentDTO;
@@ -14,9 +15,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -48,6 +50,28 @@ public class CommentServiceImpl implements CommentService {
     }
 
     public Result<String> sendComment(CommentDTO commentDTO) {
-        return ResultUtil.success("待会");
+        if (commentDTO.getInfoId() == null) {
+            return ResultUtil.error(-1, "网络错误");
+        }
+        if (Objects.equals(commentDTO.getComment(), "")) {
+            return ResultUtil.error(-1, "评论不能为空");
+        }
+        if (commentDTO.getCommentId() == null) {
+            return ResultUtil.error(-1, "网络错误");
+        }
+        if (Objects.equals(commentDTO.getCommentName(), "")) {
+            return ResultUtil.error(-1, "网络错误");
+        }
+
+        CommentEntity commentEntity = new CommentEntity();
+        BeanUtils.copyProperties(commentDTO, commentEntity);
+        Date currentDate = new Date();
+        commentEntity.setCommentTime(currentDate);
+        try {
+            commentDAO.insert(commentEntity);
+            return ResultUtil.success("评论成功");
+        } catch (Exception e) {
+            return ResultUtil.error(-1, "网络错误");
+        }
     }
 }
