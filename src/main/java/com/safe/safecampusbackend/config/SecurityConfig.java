46759ec41cdeacd6b22cd236969094e39670dab3
yawnhaaa <yawnhaaa@163.com>
2024-04-16 18:13:30 +0800

    security跨域问题解决

--- src/main/java/com/safe/safecampusbackend/config/SecurityConfig.java
+++ src/main/java/com/safe/safecampusbackend/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -17,8 +23,30 @@ public class SecurityConfig {
                 .csrf((csrf) -> csrf
                         .ignoringRequestMatchers("/**")
                 )
+                .cors((cors) -> cors
+                        .configurationSource(corsConfiguration()))
                 .addFilterBefore(new JWTFilter(), AuthorizationFilter.class);
         return http.build();
     }
 
+
+    /**
+     * Cors 的配置信息 配置+路径
+     */
+    CorsConfigurationSource corsConfiguration() {
+        // Cors配置类
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(false); // 是否返回时生成凭证
+        corsConfiguration.setAllowedHeaders(List.of("*")); // 允许请求携带哪些请求头信息
+        corsConfiguration.setAllowedMethods(List.of("*")); // 允许哪些类型的请求方法
+        corsConfiguration.setAllowedOrigins(List.of("*")); // 允许哪些域可以进行方法
+        corsConfiguration.setMaxAge(3600L); // 设置预检的最大的时长
+        corsConfiguration.setExposedHeaders(Collections.emptyList()); // 设置返回暴露的响应头信息
+
+        // 设置注册URL 配置类
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
 }
